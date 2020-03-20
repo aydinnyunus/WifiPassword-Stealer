f54e6cd53e614e4ddb483fff0a7c0ec2011a5459
Yunus AYDIN <aydinnyunus@gmail.com>
2020-03-21 00:36:23 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from cx_Freeze import setup, Executable
 
 file = open("main.py","a",encoding="utf-8")
 r1 = random.randint(-100000, 100000) 
-file.write("\n# " + r1)
+file.write("\nvar = " + r1)
+file.write("\nprint(var)\n")
+file.close()
 
 base = None    
 
