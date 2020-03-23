78b5976258f643de3643f8819d528fea941e489d
Yunus AYDIN <aydinnyunus@gmail.com>
2020-03-24 00:59:04 +0300

    Update Decrypt.py

--- Decrypt.py
+++ Decrypt.py
@@ -1,6 +1,9 @@
 from cryptography.fernet import Fernet
 
-key = " "
+key = Fernet.generate_key()
+file = open("encryption_key.txt", 'wb')
+file.write(key)
+file.close()
 
 Wifi_passwords = 'Informations.txt'
 
